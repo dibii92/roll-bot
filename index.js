2addef48949494bb431d6b24607e653b31ff9ac4
Dib Salloum <h@pop-os.localdomain>
2020-08-05 14:00:37 +0300

    initial setup

--- index.js
+++ index.js
@@ -3,24 +3,69 @@ const client = new Discord.Client();
 require("dotenv").config();
 
 let group = [];
-let group_obj = {};
+let group_obj = [];
+
+const print_t = (group, message) => {
+  if (!group.length) {
+    return message.channel.send("NO GROUP YOU MONGOL");
+  }
+  let table = "```diff\n";
+  group.forEach((el) => {
+    if (el.play === 1) {
+      table += "+ ";
+    } else if (el.play === -1) {
+      table += "- ";
+    } else {
+      table += "  ";
+    }
+    table += el.name.concat(" ").padEnd(25, "=").concat(" ") + el.roll;
+
+    if ("roll2" in el) {
+      table += " || " + el.roll2;
+    }
+    table += "\n";
+  });
+  table += "```";
+  message.channel.send(table);
+};
+
+const sort_g = (group) => {
+  group = group.sort((a, b) => b.roll - a.roll);
+};
 
 client.on("message", (message) => {
-  if (message.content === "!sign") {
-    const usr_name = message.author.username;
-
-    if (!group.includes(usr_name)) {
-      group.push(usr_name);
-      message.channel.send("Group so far : " + group);
+  if (message.content.includes("!roll") || message.content.includes("!r")) {
+    const msg = message.content;
+    const username = message.author.username;
+    if (!group.map((el) => el.name).includes(username)) {
+      group.push({
+        name: message.author.username,
+        roll: Math.floor(Math.random() * 100) + 1,
+        play: 1,
+      });
+      sort_g(group);
+      print_t(group, message);
+    }
+    if (
+      group.map((el) => el.name).includes(username) &&
+      msg.split(/(\s+)/).length > 1
+    ) {
+      group.forEach((el) => {
+        if (el.name === username) {
+          el.roll = Math.floor(Math.random() * 100) + 1;
+        }
+      });
+      sort_g(group);
+      print_t(group, message);
     }
   }
-  if (message.content === "!grouproll") {
-    group_obj = group.map((el) => ({
-      name: el,
-      roll: Math.floor(Math.random() * 100) + 1,
-    }));
-    console.log(group_obj);
-    //message.channel.send(group_obj);
+  if (message.content.includes("!end") || message.content.includes("!e")) {
+    group = [];
+    message.channel.send("END SESSION");
+  }
+  if (message.content.includes("!p") || message.content.includes("!print")) {
+    sort_g(group);
+    print_t(group, message);
   }
 });
 
